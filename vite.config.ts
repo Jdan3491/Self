957b6efe96deef552455546a30539e1328f9a28a
Jdan3491 <tvsmartdan4@gmail.com>
2024-09-13 03:41:05 +0200

    c13

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueJsx from '@vitejs/plugin-vue-jsx'
 
 export default defineConfig({
   plugins: [
@@ -9,12 +8,17 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)) // Alias per semplificare i percorsi degli import
     }
   },
   build: {
-    sourcemap: true,
+    base: '/', // Imposta la base URL per le risorse costruite
+    sourcemap: true, // Genera file di mappa per il debug
     rollupOptions: {
+      // Qui puoi configurare ulteriori opzioni di Rollup se necessario
     }
   },
+  server: {
+    // Puoi aggiungere configurazioni del server qui se necessario
+  }
 })
